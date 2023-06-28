7373e1a44d9b09449b103f263012ff9cdaf8307c
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-28 23:01:27 +0530

    Data Structure Collections

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSCollections
+{
+    public class Program
+    { 
+        public static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Different Data Structure Collection");
+        }
+    
+    
+    }
+}
